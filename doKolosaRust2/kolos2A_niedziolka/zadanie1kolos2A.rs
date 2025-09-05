001a14d4a7cd0282b7e37c70647a7940384614aa
Franciszek Chabros <franekchabr@gmail.com>
2025-09-05 14:29:10 +0200

    Update zadanie1kolos2A.rs

--- doKolosaRust2/kolos2A_niedziolka/zadanie1kolos2A.rs
+++ doKolosaRust2/kolos2A_niedziolka/zadanie1kolos2A.rs
@@ -1,55 +1,52 @@
 fn email(s: &str) -> bool {
-    // sprawdzanie znakow
+    // czy znaki sie zgadzaja
     if !s.chars().all(|c| c.is_ascii_alphanumeric() || c == '.' || c == '@') {
-        return false;
+        return false; 
     }
-
-    // pierwszy ostatni
+    // pierwszy i ostatni znak litery lub cyfry
     let first = s.chars().next().unwrap();
     let last = s.chars().last().unwrap();
     if !first.is_ascii_alphanumeric() || !last.is_ascii_alphanumeric() {
-        return false;
+        return false; 
     }
-
-    // jedna malpa
+    
+    // zliczanie jednej malpy
     let parts: Vec<&str> = s.split('@').collect();
     if parts.len() != 2 {
-        return false;
+        return false; 
     }
-
+    
     let (local, domain) = (parts[0], parts[1]);
-
-    // obie czesci nie puste
+    
+    // obie czesci nie sa puste
     if local.is_empty() || domain.is_empty() {
-        return false;
+        return false; 
     }
 
-    // po @ musi zawierac kropke i nie moze zaczynac sei ani konczyc kropka
+    // posiada kropeczni
     if !domain.contains('.') || domain.starts_with('.') || domain.ends_with('.') {
         return false;
     }
 
-    // przed @ nie moga zaczynac ani konczyc sie kropka
+    // posiada kropeczni
     if local.starts_with('.') || local.ends_with('.') {
         return false;
     }
-
+    
     true
 }
 
 fn main() {
     let test_cases = [
-        "john.doe@example.com",     // true
-        "john@examplecom",          // false (brak kropki po @)
-        ".john@example.com",        // false (zaczyna się od .)
-        "john.@example.com",        // false (kończy się na .)
-        "john@exam@ple.com",        // false (więcej niż jedna @)
-        "john@.com",                // false (domena zaczyna się od .)
-        "john.doe@ex.ample.com",    // true
+        "john.doe@example.com",        // true
+        ".siemansonMihael.@gmail.com", // false
+        "siemansonMihael@dupson.jp",   // true
+        "ejopa@prytKom",               // false
+        "_jpArmia@noSiema,xd",         // false
     ];
-
-    for addr in test_cases {
-        println!("{} -> {}", addr, email(addr));
+    
+    for adresy in test_cases {
+        println!("{} -> {}", adresy, email(adresy));
     }
 }
 
